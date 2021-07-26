e1fce4ff8109663335b88349990e07e60da536c9
Victor Wen <vicwen02@g.ucla.edu>
2021-07-26 18:04:01 -0400

    Added text for main page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Victor Wen</h1>
+      <h2>About Me</h2>
+      <p>I am a undergrad computer science major currently studying at the University of California - Los Angeles.
+      I am deeply fascinated with computer science and software engineering because I love solving difficult challenges.
+      To me, computers are all about solving problems.
+      I love working with theory and princples to logically deduce key points of some data and or a design.
+      I love the flames of creativity that inspire unique ideas and algorithms.
+      I love meticulously assembling each gear of a practical application that 
+      deploys all the theories and all the ideas from before, 
+      to craft an elegant and flexible solution.
+      But most of all, I love learning and experiencing new concepts, theories, algorithms, etc. each time I undergo this process.
+       </p>
+
+       <h2>Experience</h2>
+       <p>Almost all of my projects come from personal projects that were designed with the intent of learning something new.
+       My early expeditions into computer science involved understanding the basics of programming and object-oriented design.
+       Such projects include a simple text-based graphics engine and a short video game. Both of which developed my understanding of 
+       how an object-oriented design organizes different pieces of the program, and how those pieces became integrated together.
+       I have experience with machine learning and data science. Projects in this area include a music-genre classification app, a March Madness prediction app,
+       and a basic machine learning Java library. Recently, I have been working on various web applications. These include cookbook MERN-like website,
+       this personal website, and a AI-based research paper visualization application. Finally, the biggest project I have been working on is another video game.
+       But this time, it's sole purpose was to be large, so I could experience working with a large project and with various frameworks. The game uses the Unity game engine.
+       I have been working on and off on this project for a year, accumulating over 7,800 lines of code. Although the project is nowhere near complete nor up to industry standard,
+       I have learned a great deal about software design and engineering.</p>
     </div>
   );
 }
